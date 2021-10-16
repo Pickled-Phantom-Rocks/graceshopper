97ef0121751b520edc9577b04b62b79efbdafb89
tgpretender <jtpretender@yahoo.com>
2021-10-16 08:07:28 -0500

    roughed beginning useState on front end

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import {React, useState} from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const App = () => {
+	const [isLoggedIn, setisLoggedIn] = useState(false);
+	const [isAdmin, setIsAdmin] = useState(false);
+	const [username, setUsername] = useState('');
+	const [userToken, setUserToken] = useState('');
+
 	return <p>If you see this, Javascript is working</p>
 }
 
