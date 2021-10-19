614d50147cc02d42cdb637e676d3f4d0713f5129
Frosty <Farhan.konsulting@gmail.com>
2021-10-19 17:29:19 -0400

    fixed memory leak/admin panel select product

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {fetchProducts, fetchUsers, NewProduct, EditProduct} from './'
 
 const Admin = (props) => {
 	const {baseURL} = props;
-	const products = fetchProducts(baseURL);
-	const users = fetchUsers(baseURL);
+	const [products, setProducts] = useState([])
+	//const [users, setUsers] = useState([]);
+
+	async function fetchTheProducts() {
+
+		try {
+
+			const result = await fetchProducts(baseURL)
+			setProducts(result)
+
+		} catch (error) {
+			console.log("Error fetching products: ", error)
+		}
+
+	}
+
+	async function fetchTheUsers() {
+		try {
+
+			const result = await fetchUsers(baseURL)
+			setUsers(result)
+
+		} catch (error) {
+			console.log("Error fetching users: ", error)
+		}
+	}
 
 	async function deleteProduct(productId) {
 		console.log('Delete product ID: ', productId);
@@ -14,24 +38,38 @@ const Admin = (props) => {
 		console.log('Edit product ID: ', productId);
 	}
 
+	useEffect(() => {
+		fetchTheProducts()
+		//fetchTheUsers()
+	}, [])
+
+	function renderProducts(product) {
+		const {id, name} = product
+
+		return <option value={id} key={id}>{name}</option>
+	}
+
+	console.log(products)
+	
 	return <div className="adminPanel">
 		<h1>Admin Panel</h1>
 		<h2>Products</h2>
 		<section>
 			<button>Add New Product</button>
 			<br/><br/>
+
 			<form>
-				<select id="productSelect" size="10">
-					{
-						products.map((product) => {
-							const { id, name } = product;
-							return <option value={id} key={id}>{name}</option>
-						})
-					}
+				<label for="productLabel">Selected Product </label>
+				<select id="productSelect" >
+					<option value={'select a product'} key={0}>Select a product</option>
+					{ products.map((product) => {
+						return renderProducts(product)
+					}) }
 				</select><br/>
 				<button>Edit Product</button> 
 				<button>Delete Product</button>
 			</form>
+			
 		</section>
 		<h2>Categories</h2>
 		<section>
