723f84a887d0ff91d2e1a8ce0e8a198ba65882d8
tgpretender <jtpretender@yahoo.com>
2021-10-31 12:42:22 -0500

    restyled single product page

--- src/components/products/SingleProduct.js
+++ src/components/products/SingleProduct.js
@@ -8,18 +8,19 @@ const SingleProduct = (props) => {
 	const {name, description, quantityAvailable, price, photoName} = product;
 	const photoURL = process.env.PUBLIC_URL + "images/Products/" + photoName + ".jpg";
 
-	return <div className="singleProduct">
-		<h2>{name}</h2>
+	return <div className="productList">
+		<h3>{name}</h3>
+		<div className="productListInner">
 		<img src={photoURL} />
-		<br/>
-		<br/>
-		<label>Description:</label> {description}<br/>
-		<label>Quantity:</label> {quantityAvailable}<br/>
-		<label>Price:</label> {"$" + price}<br/>
-		<label>Category:</label> ???<br/>
-		<section className="productOptions">
+			<div className="productListInfo">
+				<label>Description:</label> {description}<br/>
+				<label>Quantity:</label> {quantityAvailable}<br/>
+				<label>Price:</label> {"$" + price}
+			</div>
+		</div>
+		<section className="userOptions">
 			<button onClick={async e => await updateUsersCart(product)}>Add to Cart</button>
-			<button>Remove from Cart</button>
+			<button  style={{marginLeft: "1em", marginTop: "1em"}} onClick={e => console.log(product)}>Remove from Cart</button>
 		</section>
 	</div>
 };
