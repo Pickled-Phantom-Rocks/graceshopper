ba7a43dcd6f66651673415269fc1bafedc643b04
Frosty <Farhan.konsulting@gmail.com>
2021-11-09 21:21:15 -0500

    prod update

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -60,7 +60,24 @@ const ProductList = (props) => {
 						</section>
 						</div>
 				} else if (quantityAvailable === 0) {
-					return
+					return <div className="productList" key={productId}>
+					<h3><Link to={ `/product/${productId}`} >{name}</Link></h3>
+					<div className="productListInner">
+						<Link to={ `/product/${productId}`} ><img src={photoURL} /></Link>
+						<div className="productListInfo">
+							<label>Description:</label> {description}<br/>
+							<label>Quantity:</label> Sold Out!<br/>
+							<label>Price:</label> {"$" + price}
+						</div>
+					</div>
+					<section className="userOptions">
+					<button onClick={async e => {
+
+						await updateUsersCart(baseURL, userId, userToken, product)
+						await fetchTheProducts()
+						}} style={{marginTop: "0.8em"}}>Add to Cart</button>
+					</section>
+					</div>
 				}
 				
 			})
