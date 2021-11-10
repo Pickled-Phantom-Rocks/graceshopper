de9b0cfa72d2bff07740dedf10e9f8b1cd7291cb
Frosty <Farhan.konsulting@gmail.com>
2021-11-09 20:49:45 -0500

    prod update

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -53,7 +53,7 @@ const ProductList = (props) => {
 					<button onClick={async e => {
 						await updateUsersCart(baseURL, userId, userToken, product)
 						await fetchTheProducts()
-						console.log("quanAvail: ", quantityAvailable)
+						console.log("quanAvail: ", quantityAvailable--)
 						}} style={{marginTop: "0.8em"}}>Add to Cart</button>
 					</section>
 				</div>
