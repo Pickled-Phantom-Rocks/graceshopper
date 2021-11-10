300c8d73d48a0c6a4c10160ab8fa524dcbec7f1f
Frosty <Farhan.konsulting@gmail.com>
2021-11-10 08:04:02 -0500

    prod update

--- src/components/products/ProductsByCategory.js
+++ src/components/products/ProductsByCategory.js
@@ -1,4 +1,4 @@
-import {React, useEffect} from 'react';
+import {React, useEffect, useState} from 'react';
 import {useParams, Link} from 'react-router-dom';
 import {fetchCategoryById} from '../categories/categoryUtils';
 import {fetchProductsByCategory} from '.';
